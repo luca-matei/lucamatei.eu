9b3dd7bb638c08c9be64758f9617d1cf87929a53
Luca Matei <dev@lucamatei.eu>
2024-03-02 16:06:39 +0200

    More content

--- src/app/(app-sections)/experience.tsx
+++ src/app/(app-sections)/experience.tsx
@@ -1,64 +1,104 @@
+import { CheckIcon } from '@heroicons/react/20/solid'
+
 const workplaces = [
   {
-    id: 1,
-    title: 'Full Stack Engineer (Contract)',
-    href: '#',
-    description:
-      'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
     period: 'Oct 2023 - Present',
-    category: { title: 'getitAI, Inc.', href: '#' },
-  },
-  {
-    id: 2,
+    company: 'getitAI, Inc.',
     title: 'Full Stack Engineer (Contract)',
-    href: '#',
-    description:
-      'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
-    period: 'Jul 2023 - Present',
-    category: { title: 'Signed NDA', href: '#' },
+    bullets: [
+      "Contributed to the development and enhancement of the knowledge base infrastructure for AI-driven sales agents, involving tasks such as refining database functionalities, optimizing storage connections, and improving search algorithms.",
+      "Assisted in the onboarding process for new team members, ensuring a smooth integration into our project environment.",
+      "Implemented a versatile global component that enables pagination, full-text search, filtering, and sorting capabilities for dynamically updated tables, enhancing user experience and data accessibility.",
+    ],
   },
   {
-    id: 3,
-    title: 'Full Stack Engineer',
-    href: '#',
-    description:
-      'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
+    period: 'Jul 2023 - Present',
+    company: 'Signed NDA',
+    title: 'Full Stack Engineer (Contract)',
+    bullets: [
+        "Actively contributed to the design and development of essential API components, ensuring robust and scalable solutions from the ground up.",
+        "Engaged in peer code reviews, providing constructive feedback to enhance code quality and team collaboration.",
+        "Led the complete redesign and implementation of a high-capacity system across multiple servers, optimizing for user-customizable request handling and efficiency.",
+        "Developed and integrated a comprehensive credits system for the API, working with third-party subscription managers like Outseta to enhance user experience.",
+        "Significantly improved API performance by overhauling its architecture, resulting in increased development speed, stability, scalability, and flexibility.",
+        "Managed various DevOps tasks, including the setup of advanced CI/CD pipelines, load balancers, and databases, and performed maintenance on both bare-metal servers and VPSes, prioritizing security and high-availability.",
+    ],
+  },
+  {
     period: 'Jun 2023 - Present',
-    category: { title: 'Freelancer', href: '#' },
+    company: 'Freelancer',
+    title: 'Full Stack Engineer',
+    bullets: [],
+  },
+]
+
+const features = [
+  {
+    name: 'Invite team members',
+    description: 'Rerum repellat labore necessitatibus reprehenderit molestiae praesentium.',
+  },
+  { name: 'List view', description: 'Corporis asperiores ea nulla temporibus asperiores non tempore assumenda aut.' },
+  {
+    name: 'Keyboard shortcuts',
+    description: 'In sit qui aliquid deleniti et. Ad nobis sunt omnis. Quo sapiente dicta laboriosam.',
+  },
+  {
+    name: 'Calendars',
+    description: 'Sed rerum sunt dignissimos ullam. Iusto iure occaecati voluptate eligendi fugiat sequi.',
+  },
+  { name: 'Notifications', description: 'Quos inventore harum enim nesciunt. Aut repellat rerum omnis adipisci.' },
+  { name: 'Boards', description: 'Quae sit sunt excepturi fugit veniam voluptatem ipsum commodi.' },
+  {
+    name: 'Reporting',
+    description: 'Eos laudantium repellat sed architecto earum unde incidunt. Illum sit dolores voluptatem.',
+  },
+  {
+    name: 'Mobile app',
+    description: 'Nulla est saepe accusamus nostrum est est. Fugit voluptatum omnis quidem voluptatem.',
   },
 ]
 
 export default function Experience() {
   return (
-    <div className="pb-24 sm:pb-32">
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-          {workplaces.map((workplace) => (
-            <article key={workplace.id} className="flex max-w-xl flex-col items-start justify-between">
-              <div className="flex items-center gap-x-4 text-xs">
-                <span className="text-gray-500">
-                  {workplace.period}
-                </span>
-                <a
-                  href={workplace.category.href}
-                  className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
-                >
-                  {workplace.category.title}
-                </a>
-              </div>
-              <div className="group relative">
-                <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
-                  <a href={workplace.href}>
-                    <span className="absolute inset-0" />
-                    {workplace.title}
-                  </a>
-                </h3>
-                <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">{workplace.description}</p>
-              </div>
-            </article>
-          ))}
+    <>
+      <div className="pt-24 sm:pt-32">
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <div className="mx-auto max-w-2xl lg:mx-0">
+            <h2 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Places I've Worked</h2>
+            <p className="mt-6 text-lg leading-8 text-gray-600">
+              I'm known for my resilience, self-reliance and strong commitment to growth.
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+      <div className="py-24 sm:py-32">
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <div
+              className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+            <div>
+              <h2 className="text-base font-semibold leading-7 text-indigo-600">getitAI, Inc.</h2>
+              <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Full Stack Engineer</p>
+              <h3 className="mt-4 text-sm leading-7 text-gray-600">October 2023 - Present</h3>
+              <h3 className="text-sm leading-7 text-gray-600">Remote, Contract</h3>
+              <p className="mt-4 text-base leading-7 text-gray-600">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque,
+                iste
+                dolor cupiditate blanditiis ratione.
+              </p>
+            </div>
+            <dl className="col-span-2 grid grid-cols-1 gap-x-8 gap-y-10 text-base leading-7 text-gray-600 sm:grid-cols-2 lg:gap-y-16">
+                {features.map((feature) => (
+                  <div key={feature.name} className="relative pl-9">
+                    <dt>
+                      <CheckIcon className="absolute left-0 top-1 h-5 w-5 text-indigo-500" aria-hidden="true" />
+                      {feature.description}
+                    </dt>
+                  </div>
+                ))}
+              </dl>
+            </div>
+          </div>
+        </div>
+    </>
   )
 }
--- src/app/(app-sections)/skills.tsx
+++ src/app/(app-sections)/skills.tsx
@@ -14,55 +14,56 @@ const features = [
   {
     name: 'Backend',
     description:
-      'I work with REST APIs on a daily basis. In 2020 I built a framework on top of a uWSGI server to serve the backend of multiple websites.',
-    tags: ['fastapi', 'celery', 'dramatiq', 'pydantic', 'sqlalchemy', 'flask', 'django', 'bottle.py'],
+      'I work with ASGI REST APIs to power SaaS platforms on a daily basis. In 2020 I built a framework on top of a uWSGI server to serve the backend of multiple presentation websites.',
+    tags: ['fastapi', 'postgresql', 'redis', 'celery', 'dramatiq', 'supabase', 'sqlalchemy', 'pydantic'],
   },
   {
-    name: 'Full Stack',
+    name: 'Frontend',
     description:
-      'Magnam provident veritatis odit. Vitae eligendi repellat non. Eum fugit impedit veritatis ducimus. Non qui aspernatur laudantium modi. Praesentium rerum error deserunt harum.',
-    tags: ['Linux', 'Bash', 'SSH', 'Nginx', 'Apache', 'Docker', 'Kubernetes'],
+      "While I'm more focused on backend, I can build user interfaces and wire components to the backend. In 2020 I built a CSS framework from the ground up to style my websites in a manner similar to what Tailwind CSS has to offer.",
+    tags: ['nextjs', 'react', 'typescript', 'javascript', 'tailwindcss', 'bootstrap', 'material', 'sass'],
   },
   {
     name: 'DevOps',
     description:
-      'Sit minus expedita quam in ullam molestiae dignissimos in harum. Tenetur dolorem iure. Non nesciunt dolorem veniam necessitatibus laboriosam voluptas perspiciatis error.',
-    tags: ['Linux', 'Bash', 'SSH', 'Nginx', 'Apache', 'Docker', 'Kubernetes'],
-  },
-  {
-    name: 'Open Source',
-    description:
-      'Ipsa in earum deserunt aut. Quos minus aut animi et soluta. Ipsum dicta ut quia eius. Possimus reprehenderit iste aspernatur ut est velit consequatur distinctio.',
-    tags: ['Linux', 'Bash', 'SSH', 'Nginx', 'Apache', 'Docker', 'Kubernetes'],
+      'I can set up and maintain linux, web and database servers and containers with robust load balancing. I have experience in setting up CI/CD pipelines and monitors.',
+    tags: ['railway', 'github-actions', 'bitbucket-pipelines', 'hetzner', 'haproxy', 'pgbouncer', 'git', 'docker', 'prometheus', 'grafana', 'sentry', 'supervisor'],
   },
 ]
 
 export default function Skills() {
   return (
-    <div className="mx-auto max-w-7xl px-6 lg:px-8">
-      <div className="mx-auto max-w-2xl lg:mx-0">
-        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Skills I've Gained</h2>
-        <p className="mt-6 text-lg leading-8 text-gray-600">
-          Here's what I've learned in 13 years of writing code and what I brought into my recent 6 months of professional work.
-        </p>
-      </div>
-      <dl className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 text-base leading-7 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-        {features.map((feature) => (
-          <div key={feature.name}>
-            <dt className="font-semibold text-gray-900">{feature.name}</dt>
-            <dd className="my-1 text-gray-600">{feature.description}</dd>
-            <div className="flex flex-row flex-wrap">
-              {feature.tags.map((tag) => (
-                <span
-                  className="relative z-10 rounded-full bg-gray-50 px-2 py-1 mt-2 mr-2 text-sm text-gray-500 hover:bg-gray-100"
-                >
-                  #{tag}
-                </span>
-              ))}
-            </div>
+    <>
+      <div className="pt-24 sm:pt-32 lg:pt-40">
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <div className="mx-auto max-w-2xl lg:mx-0">
+            <h2 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Skills I've Gained</h2>
+            <p className="mt-6 text-lg leading-8 text-gray-600">
+              Here's what I've learned in 13 years of writing code and what I brought into my recent 6 months of professional
+            work.
+            </p>
           </div>
-        ))}
-      </dl>
-    </div>
+        </div>
+      </div>
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <dl className="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 text-base leading-7 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+          {features.map((feature) => (
+            <div key={feature.name}>
+              <dt className="font-semibold text-gray-900">{feature.name}</dt>
+              <dd className="my-1 text-gray-600">{feature.description}</dd>
+              <div className="flex flex-row flex-wrap">
+                {feature.tags.map((tag) => (
+                  <span
+                    className="relative z-10 rounded-full bg-gray-50 px-2 py-1 mt-2 mr-2 text-sm text-gray-500 hover:bg-gray-100"
+                  >
+                    #{tag}
+                  </span>
+                ))}
+              </div>
+            </div>
+          ))}
+        </dl>
+      </div>
+    </>
   )
 }
