6534b403c465a47fd5b108b0332447d24f8be713
Luca Matei <dev@lucamatei.eu>
2024-07-24 01:13:33 +0300

    Update services

--- src/app/(presentation-pages)/(home)/services.tsx
+++ src/app/(presentation-pages)/(home)/services.tsx
@@ -6,7 +6,7 @@ const tiers = [
     name: "Software Consulting",
     id: "tier-consulting",
     href: "#contact",
-    priceMonthly: "€39",
+    priceHourly: "€19",
     description: "Ideal for individuals and small teams ready to kickstart their projects or overcome hurdles.",
     features: [
       "45-Minute Free Consultation",
@@ -18,11 +18,11 @@ const tiers = [
     name: "Custom Software Development",
     id: "tier-development",
     href: "#contact",
-    priceMonthly: "€59",
+    priceHourly: "€29",
     description: "Perfect for those who need another hand on deck. I work best in fast-paced startup environments.",
     features: [
       "No onboarding fees",
-      "Free bug fixes or money back",
+      "Free post-collaboration bug fixes or money back",
       "Reliable, scalable, and self-documenting code",
     ],
   },
@@ -34,8 +34,8 @@ export default function Services() {
     title: () => {return (<>Let&apos;s Create <b>Your</b> Next Big Hit!</>);},
     description: "I focus on developing Python APIs for SaaS products, but I'm also skilled in managing Linux servers, integrating with Shopify, and working with Large Language Models.",
     discounted: {
-      title: "Discounted",
-      description: "Educational or social impact projects may qualify for a discount or pro-bono work. Other projects can get a lower rate if handled by the juniors I train, as I also have a full-time job or the projects are lower priority."
+      title: "Discounts",
+      description: "Educational or social impact projects are guaranteed to qualify for a discount or pro-bono work."
     }
   }
   return (
@@ -75,7 +75,7 @@ export default function Services() {
                       {tier.name}
                     </h3>
                     <div className="mt-4 flex items-baseline gap-x-2">
-                      <span className="text-5xl font-bold tracking-tight text-gray-900">{tier.priceMonthly}</span>
+                      <span className="text-5xl font-bold tracking-tight text-gray-900">{tier.priceHourly}</span>
                       <span className="text-base font-semibold leading-7 text-gray-600">/hour</span>
                     </div>
                     <p className="mt-6 text-base leading-7 text-gray-600">{tier.description}</p>
@@ -93,20 +93,18 @@ export default function Services() {
                     aria-describedby={tier.id}
                     className="mt-8 block rounded-md bg-indigo-600 px-3.5 py-2 text-center text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                   >
-                    Get started today
+                    Book a meeting
                   </a>
                 </div>
               ))}
-              <div className="flex flex-col items-start gap-x-8 gap-y-6 rounded-3xl p-8 ring-1 ring-gray-900/10 sm:gap-y-10 sm:p-10 lg:col-span-2 lg:flex-row lg:items-center">
-                <div className="lg:min-w-0 lg:flex-1">
-                  <h3 className="text-lg font-semibold leading-8 tracking-tight text-indigo-600">{content.discounted.title}</h3>
-                  <p className="mt-1 text-base leading-7 text-gray-600">
-                    {content.discounted.description}
-                  </p>
-                </div>
+              <div className="lg:col-span-2 full rounded-3xl p-8 ring-1 ring-gray-900/10 sm:p-10">
+                <h3 className="text-lg font-semibold leading-8 tracking-tight text-indigo-600">{content.discounted.title}</h3>
+                <p className="mt-1 text-base leading-7 text-gray-600">
+                  {content.discounted.description}
+                </p>
                 <a
                   href="#contact"
-                  className="rounded-md px-3.5 py-2 text-sm font-semibold leading-6 text-indigo-600 ring-1 ring-inset ring-indigo-200 hover:ring-indigo-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  className="inline-block rounded-md mt-4 px-3.5 py-2 text-sm font-semibold leading-6 text-indigo-600 ring-1 ring-inset ring-indigo-200 hover:ring-indigo-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
                   Book a meeting <span aria-hidden="true"><FontAwesomeIcon icon={faLongArrowRight} className="ml-1 w-3 h-3" /></span>
                 </a>
