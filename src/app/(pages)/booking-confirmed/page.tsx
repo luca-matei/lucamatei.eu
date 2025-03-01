a43bda0d31cb1d3eef4e5ec2204ff249d2f7e1bc
Luca Matei <dev@lucamatei.eu>
2025-03-01 21:38:14 +0100

    chore: add booking confirmed page

--- src/app/(pages)/booking-confirmed/page.tsx
+++ src/app/(pages)/booking-confirmed/page.tsx
@@ -1,8 +1,16 @@
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faCircleCheck} from "@fortawesome/free-solid-svg-icons";
 
 export default function BookingConfirmedPage() {
   return (
-    <div>
-      <h1>Booking Confirmed</h1>
+    <div className={"px-6 py-32 sm:px-6 sm:py-48 lg:px-8 bg-gradient-to-r from-green-500 to-emerald-600"}>
+      <div className={"max-w-3xl mx-auto text-center text-white"}>
+        <h1 className={"mb-2 flex justify-center items-center text-4xl sm:text-5xl"}>
+          Booking Confirmed <FontAwesomeIcon icon={faCircleCheck}
+                                             className={"w-8 h-8 text-green-500 bg-white ml-2 p-0.5 rounded-full inline-block"}/>
+        </h1>
+        <p className={"font-semibold text-white/90"}>All set! Your call is booked. Looking forward to our call!</p>
+      </div>
     </div>
   );
 }
