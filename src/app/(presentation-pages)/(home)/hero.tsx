f24fbf01a63d881e368e10b02d937dc5d7488aa5
Luca Matei <dev@lucamatei.eu>
2025-03-13 02:36:07 +0200

    fix: updated role title

--- src/app/(presentation-pages)/(home)/hero.tsx
+++ src/app/(presentation-pages)/(home)/hero.tsx
@@ -3,9 +3,9 @@ import {faArrowRightLong} from "@fortawesome/free-solid-svg-icons";
 
 export default function Hero() {
   const content = {
-    subtitle: "Full Stack Engineer",
+    subtitle: "Senior Full Stack Engineer",
     title: () => {
-      return <>Hi, I&apos;m Luca,<br /><span className="text-indigo-600">Full Stack Engineer</span></>
+      return <>Hi, I&apos;m Luca,<br /><span className="text-indigo-600">Senior Full Stack Engineer</span></>
     },
     description: "Experienced impact-driven engineer specializing in B2B startup environments. Creator of open-source IaC tool, WSGI web framework, and utility-first CSS library. Authored technical guides for various software products. Proficient in Python, TypeScript, and Linux. Mentor for students and junior developers in procedural programming, web development, and LLMs. Open-source contributor to Pydantic. Built high-throughput e-commerce products in global, agile teams. Certified ScrumMaster with multiple Coursera certifications in full-stack development, AI, and Rust. Pursuing AWS certification.",
   }
